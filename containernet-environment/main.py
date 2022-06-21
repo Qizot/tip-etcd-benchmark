83f084cdb6ca184b0367ef36e0af99be65fbec4f
Qizot <qizot1405@gmail.com>
2022-06-21 21:14:14 +0200

    Modify config to support arbitrary number of etcd instances

--- containernet-environment/main.py
+++ containernet-environment/main.py
@@ -2,97 +2,89 @@
 
 # os.environ["XTABLES_LIBDIR"] = "/home/qizot/iptables-tmp"
 import sys
+import json
+import argparse
+
 from mininet.net import Containernet
 from mininet.node import Controller
 from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import info, setLogLevel, error
 from mininet.clean import cleanup
-setLogLevel('info')
-if len(sys.argv) != 2:
-    error("Pass mode as a command line argument!\n USAGE: sudo python3 main.py [cluster|standalone]\n")
-    sys.exit()
-mode = sys.argv[1]
-cluster_mode = mode == "cluster"
-standalone_mode = mode == "standalone"
 
-if not cluster_mode and not standalone_mode:
-    error("Invalid mode passed! Available modes: cluster | standalone\n")
-    sys.exit()
+def setup_etcd_instances(net, switch, config_file_path):
+    configs = json.loads(config_file_path)
+    print(configs)
+
+    if type(configs) != list or len(configs) < 1:
+        raise RuntimeError("The config file should be a json array containing at least one instance")
+
+
+    print(config_file_path)
+    initial_cluster = ",".join([f"ectd_{idx}=http://{node['ip']}:2380" for idx, node in enumerate(configs)])
+
+    instances = []
+
+    for idx, instance_cfg in enumerate(configs): 
+        name = f"etcd_{idx}"
+        ip = instance_cfg["ip"]
+        # string representation of memory e.g. 100mb
+        mem = instance_cfg["mem"]
+        # fractional number of CPUs cores to use
+        cpus = float(instance_cfg["cpus"])
+        # bandwidth should be given in Mbps
+        bandwidth = int(instance_cfg["bandwidth"])
+        # a delay string e.g. '30ms'
+        link_delay = instance_cfg["link_delay"]
+
+        instance = net.addDocker(
+            name,
+            ip=ip,
+            dimage="etcd_custom",
+            environment={
+                "ETCD_NAME": name, 
+                "ALLOW_NONE_AUTHENTICATION": "yes",
+                "ETCD_INITIAL_CLUSTER_TOKEN": "etcd-cluster",
+                "ETCD_INITIAL_CLUSTER_STATE": "new",
+                # ip related config variables
+                "ETCD_ADVERTISE_CLIENT_URLS": f"http://{ip}:2379",
+                "ETCD_INITIAL_ADVERTISE_PEER_URLS": f"http://{ip}:2380",
+                "ETCD_LISTEN_PEER_URLS": "http://0.0.0.0:2380",
+                "ETCD_LISTEN_CLIENT_URLS": "http://0.0.0.0:2379",
+
+                "ETCD_INITIAL_CLUSTER": initial_cluster, 
+            },
+            ports=[2379, 2380],
+            dcmd='/opt/bitnami/scripts/etcd/run.sh',
+            cpus=cpus,
+            memor=mem,
+        )
+        instances.append(instance)
+
+        net.addLink(instance, switch, cls=TCLink, delay=link_delay, bw=bandwidth)
+
+
+parser = argparse.ArgumentParser(
+    description="Containernet ETCD benchmarking environment",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument("-c", "--config-file", type=str, required=True, help="etcd instances configuration file")
+args = parser.parse_args()
+
+setLogLevel('info')
 
 cleanup()
+
 net = Containernet(controller=Controller)
 info('*** Adding controller\n')
 net.addController('c0')
 
-info('*** Adding ETCD containers\n')
-etcd1 = net.addDocker(
-    'etcd1', 
-    ip='10.0.0.251', 
-    dimage="etcd_custom",
-    environment={
-        'ETCD_ADVERTISE_CLIENT_URLS': 'http://10.0.0.251:2379',
-        "ALLOW_NONE_AUTHENTICATION": "yes",
-        "ETCD_NAME": "etcd1",
-        "ETCD_INITIAL_ADVERTISE_PEER_URLS": "http://10.0.0.251:2380",
-        "ETCD_LISTEN_PEER_URLS": "http://0.0.0.0:2380",
-        "ETCD_LISTEN_CLIENT_URLS": "http://0.0.0.0:2379",
-        "ETCD_INITIAL_CLUSTER_TOKEN": "etcd-cluster",
-        "ETCD_INITIAL_CLUSTER": "etcd1=http://10.0.0.251:2380,etcd2=http://10.0.0.253:2380,etcd3=http://10.0.0.254:2380",
-        "ETCD_INITIAL_CLUSTER_STATE": "new"
-    } if cluster_mode else {
-        'ETCD_ADVERTISE_CLIENT_URLS': 'http://10.0.0.251:2379',
-        "ALLOW_NONE_AUTHENTICATION": "yes",
-    },
-    ports=[2379, 2380],
-    dcmd='/opt/bitnami/scripts/etcd/run.sh'
-)
-
-if cluster_mode:
-    etcd2 = net.addDocker(
-        'etcd2', 
-        ip='10.0.0.253', 
-        dimage="etcd_custom",
-        environment={
-            'ETCD_ADVERTISE_CLIENT_URLS': 'http://10.0.0.253:2379',
-            "ALLOW_NONE_AUTHENTICATION": "yes",
-            "ETCD_NAME": "etcd2",
-            "ETCD_INITIAL_ADVERTISE_PEER_URLS": "http://10.0.0.253:2380",
-            "ETCD_LISTEN_PEER_URLS": "http://0.0.0.0:2380",
-            "ETCD_LISTEN_CLIENT_URLS": "http://0.0.0.0:2379",
-            "ETCD_INITIAL_CLUSTER_TOKEN": "etcd-cluster",
-            "ETCD_INITIAL_CLUSTER": "etcd1=http://10.0.0.251:2380,etcd2=http://10.0.0.253:2380,etcd3=http://10.0.0.254:2380",
-            "ETCD_INITIAL_CLUSTER_STATE": "new"
-        },
-        ports=[2379, 2380],
-        dcmd='/opt/bitnami/scripts/etcd/run.sh'
-    )
-
-    etcd3 = net.addDocker(
-        'etcd3', 
-        ip='10.0.0.254', 
-        dimage="etcd_custom",
-        environment={
-            'ETCD_ADVERTISE_CLIENT_URLS': 'http://10.0.0.254:2379',
-            "ALLOW_NONE_AUTHENTICATION": "yes",
-            "ETCD_NAME": "etcd3",
-            "ETCD_INITIAL_ADVERTISE_PEER_URLS": "http://10.0.0.254:2380",
-            "ETCD_LISTEN_PEER_URLS": "http://0.0.0.0:2380",
-            "ETCD_LISTEN_CLIENT_URLS": "http://0.0.0.0:2379",
-            "ETCD_INITIAL_CLUSTER_TOKEN": "etcd-cluster",
-            "ETCD_INITIAL_CLUSTER": "etcd1=http://10.0.0.251:2380,etcd2=http://10.0.0.253:2380,etcd3=http://10.0.0.254:2380",
-            "ETCD_INITIAL_CLUSTER_STATE": "new"
-        },
-        ports=[2379, 2380],
-        dcmd='/opt/bitnami/scripts/etcd/run.sh'
-    )
-
-
 info('*** Adding benchmark containers')
 
-benchmark1 = net.addDocker(
-    'benchmark1', 
-    ip='10.0.0.252', 
+benchmark = net.addDocker(
+    'benchmark', 
+    ip='10.0.0.250', 
     dimage='etcd_benchmark',
     environment={
       "BENCHMARK_EXEC": "/bin/benchmark-client"
@@ -102,16 +94,21 @@ benchmark1 = net.addDocker(
     port_bindings={8080:8080}
 )
 
+info('*** Adding ETCD containers\n')
+
+
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
 s2 = net.addSwitch('s2')
-info('*** Creating links\n')
-net.addLink(etcd1, s1)
-if cluster_mode:
-    net.addLink(etcd2, s1)
-    net.addLink(etcd3, s1)
+
+info("*** Creating ETCD instances\n")
+
+setup_etcd_instances(net, s1, args.config_file)
+
+info('*** Connecting switches\n')
+
 net.addLink(s1, s2, cls=TCLink, delay='100ms', bw=1)
-net.addLink(s2, benchmark1)
+net.addLink(s2, benchmark)
 
 info('*** Starting network\n')
 net.start()
